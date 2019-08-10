c260ae09a1be2cd6336bd898a12f9f2c139a52b9
zl64183810aaa <52234178+zl64183810aaa@users.noreply.github.com>
2019-08-10 17:03:14 +0800

    8/10

--- src/views/reg.vue
+++ src/views/reg.vue
@@ -8,11 +8,22 @@
             <h1 class="title">听你想听，做你想做</h1>
             <h2 class="welcome">欢迎注册</h2>
             <div class="info">
-                <input type="text" placeholder="手机" v-model="phone">
-                <input type="password" placeholder="密码" v-model="mima" >
-                <input class="yes" type="checkbox" ><span>我已阅读并同意相关服务条款</span>
-                <button :disabled="!isAgree">注册</button> 
-                <img src="../../public/img/login/zhengque.png">    
+                <input type="text" placeholder="手机" v-model="phone" @blur="ph">
+                <input type="password" placeholder="密码" v-model="mima" @blur="mi"> 
+                <input class="yes" type="checkbox" v-model="isAgree" ><span>我已阅读并同意相关服务条款</span>
+                <button >注册</button> 
+                <div v-show="yes">
+                <img class="ver" src="../../public/img/login/zhengque.png">
+                 </div>   
+                 <div v-show="no">
+                <img class="ver" src="../../public/img/login/cuowu.png">
+                 </div>
+                  <div v-show="myes">
+                 <img class="vermi" src="../../public/img/login/zhengque.png">
+                 </div>   
+                 <div v-show="mno">
+                <img class="vermi" src="../../public/img/login/cuowu.png">
+                 </div> 
                 <img class="mima" src="../../public/img/login/mima.png" >
             </div>
             <h4 class="cop">Copyright © 1998-2019Tencent All Rights Reserved</h4>
@@ -29,26 +40,58 @@
 export default {
     data(){
         return {
+           isAgree:true,
            phone:"",
-           mima:""
+           mima:"",
+           yes:false,
+           no:false,
+           myes:false,
+           mno:false,
         }
     },
     methods:{
-        phone(){
+        ph(){
             var reg=/^1[3-9]\d{9}$/;
             if(reg.test(this.phone)){
-                      
+                   this.yes=true 
+                   this.no=false  
+            }else{
+                this.yes=false 
+                this.no=true 
+            }
+        },
+         mi(){
+            var reg=/^[a-z0-9]{3,9}$/i;
+            if(reg.test(this.mima)){
+                   this.myes=true 
+                   this.mno=false  
+            }else{
+                this.myes=false 
+                this.mno=true 
             }
         }
     }
 }
 </script>
 <style scoped>
-.mima{
-    width: 30px;
+.vermi{
+    width: 40px;
     position: absolute;
-    top:24%;
-    left: 87%;
+    top:23%;
+    left: 85%;
+    z-index: 1;
+}
+.ver{
+     width: 40px;
+    position: absolute;
+    top:6%;
+    left: 85%;
+}
+.mima{
+    width: 23px;
+    position: absolute;
+    top:25%;
+    left: 88%;
 }
 .reg-bg{
    width:1535px;
@@ -146,7 +189,7 @@ export default {
     color: #333;
     position: absolute;
     top:600px;
-    left:850px;
+    left:750px;
 }
 @keyframes star{
     0%{opacity:0;transform:translate(0px,0px)}
